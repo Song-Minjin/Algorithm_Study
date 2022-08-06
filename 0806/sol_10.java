9bea1f8536123c074eed991ec9baa8975cc34ad7
jinu-ahn <66781422+jinu-ahn@users.noreply.github.com>
2022-08-06 11:50:04 +0900

    Problem_10 Solution by jinwoo

--- 0806/sol_10.java
+++ 0806/sol_10.java
@@ -2,8 +2,11 @@
 
 class Solution {
     public int[][] solution(int[][] arr1, int[][] arr2) {
+        
+        // answer 2중배열에 크기 할당
         int[][] answer = new int[arr1.length][arr1[0].length];
-
+        
+        // 2중 for문으로 arr1[0][0] 과 arr2[0][0] 부터 arr1의 배열 크기만큼 인덱스를 올려 더하기
         for(int i=0; i < arr1.length; i++){
             for(int j=0; j < arr1[i].length; j++){
                 answer[i][j] = arr1[i][j] + arr2[i][j];
