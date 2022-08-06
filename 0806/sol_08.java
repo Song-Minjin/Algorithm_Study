b4362cc51ea856ed1d0b94b60ef01f167c72d629
jinu-ahn <66781422+jinu-ahn@users.noreply.github.com>
2022-08-06 11:45:51 +0900

    Problem_08 Solution by jinwoo

--- 0806/sol_08.java
+++ 0806/sol_08.java
@@ -1,8 +1,11 @@
 class Solution {
     public double solution(int[] arr) {
         double answer = 0;
+        
+        // for each문으로 i에 arr의 값 넣고 answer에 더하기
             for(int i : arr)
                 answer += i;
+        // answer에 arr의 길이만큼 나누기
             return answer/arr.length;
     }
 }
