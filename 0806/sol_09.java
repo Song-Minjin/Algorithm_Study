84a7717a5f0d9ea58f94ff87c519fcdf0b80f27c
jinu-ahn <66781422+jinu-ahn@users.noreply.github.com>
2022-08-06 11:47:37 +0900

    Problem_09 Solution by jinwoo

--- 0806/sol_09.java
+++ 0806/sol_09.java
@@ -1,13 +1,14 @@
-저는 tostring으로 문자열을 끊어서 해결하였습니다.
+저는 substring으로 문자열을 끊어서 해결하였습니다.
 
 class Solution {
     public String solution(String phone_number) {
         String answer = "";
         
-        
+        // phone_number 길이의 -4까지 반복하여 "*"을 찍어 answer에 추가
         for(int i=0; i<phone_number.length()-4;i++){
             answer +="*";
         }
+        // substring으로 phone_number의 마지막 4자리를 끊어 answer에 추가
         answer += phone_number.substring(phone_number.length()-4);
         
         
