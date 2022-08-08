07bfadd8538da5ba17e6b4c6a425225f7e2bda69
송민진 <m_6595@naver.com>
2022-08-08 13:08:04 +0900

    2022-08-08 파일명 변경

--- 22.08.06/Problem10_송민진.java
+++ 22.08.06/Problem10_송민진.java
@@ -1,6 +1,6 @@
 class Solution {
     public int[][] solution(int[][] arr1, int[][] arr2) {
-        int[][] answer = new int [arr1.length][arr1[0].length];     // Index 0 out of bounds (arr1,arr2의 길이가 0일 때) 방지용 선언
+        int[][] answer = new int [arr1.length][arr1[0].length];     // 배열 길이 할당 (안 할 시 Index 0 out of bounds 오류 발생)
 
         for (int i = 0; i < arr1.length; i++) {
             for (int j = 0; j < arr1[i].length; j++) {
