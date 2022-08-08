19a459119f268c7c21ea90f0b4237e503ab31193
송민진 <m_6595@naver.com>
2022-08-08 22:39:39 +0900

    2022-08-08 임시 업로드

--- Problem12_송민진.java
+++ Problem12_송민진.java
@@ -2,6 +2,7 @@ class Solution {
     public long solution(int price, int money, int count) {
         long total_cost = 0;
         int new_price = price;
+
         for(int i = 0; i < count; i++){
             total_cost += new_price;
             new_price += price;
