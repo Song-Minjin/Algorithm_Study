523a249a8c74cccbe09bfa1d4273fe02b52af269
jinu-ahn <66781422+jinu-ahn@users.noreply.github.com>
2022-08-08 14:26:17 +0900

    Problem_18 Solution by jinwoo

--- 0808/sol_18.java
+++ 0808/sol_18.java
@@ -12,17 +12,3 @@ class Solution {
         return answer;
     }
 }
-
------------------------------------------------------------------------------
-  List 사용
-  
-import java.util.Arrays;
-
-public class FindKim {
-    public String findKim(String[] seoul){
-        //x에 김서방의 위치를 저장하세요.
-        int x = Arrays.asList(seoul).indexOf("Kim");
-
-        return "김서방은 "+ x + "에 있다";
-    }
-}
