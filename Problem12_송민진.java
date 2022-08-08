dda12284ba13572779cec6524c8b3847aa00a11b
송민진 <m_6595@naver.com>
2022-08-08 14:35:08 +0900

    2022-08-08 민진 문제 11,12,15 풀이 추가

---
+++ Problem12_송민진.java
@@ -0,0 +1,15 @@
+class Solution {
+    public long solution(int price, int money, int count) {
+        long total_cost = 0;
+        int new_price = price;
+        for(int i = 0; i < count; i++){
+            total_cost += new_price;
+            new_price += price;
+        }
+
+        if (total_cost <= money){
+            return 0;
+        }
+        return total_cost - money;
+    }
+}
